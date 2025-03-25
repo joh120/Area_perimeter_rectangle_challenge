91b12b168adcac7f12322997a71076896692bf17
joh120 <gatjohnn1@gmail.com>
2025-03-25 13:05:41 +0000

    Add files via upload

---
+++ area_perimeter_rectangle_coding_challenge.js
@@ -0,0 +1,64 @@
+/*
+
+Area and Perimeter of a Rectangle
+The program should:
+
+– Prompt the user to enter the length and width of the rectangle.
+– Calculate the area and perimeter of the rectangle using the length and width entered by the user.
+
+*/
+
+let length = Number(window.prompt("Please enter length of the rectangle"))
+let width = Number(window.prompt("Please enter width of the rectangle"))
+
+let area = length * width
+let perimeter = (length + width) * 2
+
+console.log(area) // prints -> 100
+console.log(perimeter) // prints -> 40
+
+// End of area and perimeter of rectangle challenge 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   `${user.getName()} lives in ${user.getLocation()} and was born on ${user.dob}.`
